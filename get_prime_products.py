ca37620248dca558a82269b3a914acc7f391ab1c
EliasKomar <elias.komar@ctemplar.com>
2020-09-11 00:01:21 +0200

    added results database

--- get_prime_products.py
+++ get_prime_products.py
@@ -1,15 +1,28 @@
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium import webdriver
-from time import sleep
+from random import randint
 import geckodriver_autoinstaller
 
+
+batch_size = 5
+min_length = 8
+max_length = 250
+number_file = 'primeproducts.txt'
 
 geckodriver_autoinstaller.install()  # Check if the current version of geckodriver exists
                                      # and if it doesn't exist, download it automatically,
                                      # then add geckodriver to path
 
-#options = FirefoxOptions()
-#options.add_argument("--headless")
-#driver = webdriver.Firefox(options=options)
+def file_len(filename):
+    with open(filename) as f:
+        i = None
+        for i, l in enumerate(f):
+            pass
+        if i is None:
+            print('file empty')
+            return 0
+    return i + 1
 
 def get_number(bit_len=128):
     binary_len = driver.find_element_by_id('prime')
@@ -19,9 +32,70 @@ def get_number(bit_len=128):
     submit.click()
     return driver.find_element_by_id('random3').get_property('value')
 
+def make_int(num):
+    out_num = ''
+    while len(num) > 3:
+        out_num = num[-3:] + out_num
+        num = num[:-4]
+    return int(num + out_num)
+
+def export_prime_products(prime_products, number_file=number_file):
+    with open(number_file, 'a') as f:
+        for prime_product in prime_products:
+            f.write(prime_product + '\n')
+
+def main(batch_size=batch_size, max_length=max_length, min_length=min_length, number_file=number_file):
+    file_length = file_len(number_file)
+    for i in range(min_length + (file_length//batch_size), max_length + 1):
+        numbers = []
+        bit_length = i
+        for j in range(batch_size):
+            # rather hacky but it helpes and the programm almost never crashes
+            try:
+                numbers.append(str(make_int(get_number(bit_len=bit_length))))
+            except:
+                try:
+                    print('request failed - trying again...')
+                    numbers.append(str(make_int(get_number(bit_len=bit_length))))
+                except:
+                    print('failed twice')
+                    exit()
+        export_prime_products(numbers)
+        print('batch[' + str(i-min_length+1) + '/' + str(max_length-min_length+1) + ']')
+
+def sort_em(number_file=number_file):
+    with open(number_file, 'r') as f:
+        numbers = [int(line.strip('\n')) for line in f.readlines()]
+    numbers = sorted(numbers)
+    i = 1
+    while i < len(numbers) - 1:
+        if numbers[i] == numbers[i-1]:
+            del numbers[i]
+        i += 1
+    numbers = [str(number) for number in numbers]
+    open(number_file, 'w').close()
+    export_prime_products(numbers)
+
+
+
 if __name__ == '__main__':
-    driver = webdriver.Firefox()
-    driver.get('https://asecuritysite.com/encryption/random3')
-    print(get_number())
-    sleep(5)
-    driver.quit()
+    try:
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+
+        print('accessing https://asecuritysite.com/encryption/random3')
+        driver.get('https://asecuritysite.com/encryption/random3')
+
+        print('fetching content...')
+        main()
+
+        print('sorting the numbers by size...')
+        sort_em()
+
+        driver.quit()
+    finally:
+        try:
+            brower.close()
+        except:
+            pass
--- speedtest.py
+++ speedtest.py
@@ -1,38 +1,52 @@
 #!/usr/bin/env python3
-from lenstra_algorithm.lenstra import lenstra
 from time import time
 from datetime import datetime
+from lenstra_algorithm.lenstra import lenstra
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime, BigInteger
 
+#########################################
 
+batch_size = 5
 input_file = 'primeproducts.txt'
-output_file = 'results.txt'
-batch_size = 2
 device = 'MacBook Pro (Mid 2014): 2.8 GHz Dual-Core Intel Core i5'
+
+#########################################
+
+db = create_engine('sqlite:///VWA-Algorithmus_Speedtest.db')
+db.connect()
+metadata = MetaData()
+
+results = Table('results', metadata,
+   Column('number', String, nullable=False),
+   Column('prime1', String, nullable=False),
+   Column('prime2', String, nullable=False),
+   Column('ecm', String, nullable=False),
+   Column('device', String, default=device),
+   Column('time_added', DateTime, default=datetime.utcnow))
+
+metadata.create_all(db)
+
+#########################################
+
 
 def import_batch(input_file=input_file, batch_size=batch_size):
     # get batch from primeproducts.txt
     with open(input_file, 'r') as f:
         return [int(line.strip('\n')) for line in f.readlines()[:batch_size]]
 
-# https://stackoverflow.com/questions/845058/how-to-get-line-count-of-a-large-file-cheaply-in-python
-# ==================================================================================================
-def file_len(fname):
-    with open(fname) as f:
-        #i = None
+def file_len(filename):
+    with open(filename) as f:
+        i = None
         for i, l in enumerate(f):
             pass
-        if i is None:
-            print('file empty')
-            return 0
     return i + 1
-# ==================================================================================================
 
-def export_results(results, output_file=output_file, input_file=input_file, batch_size=batch_size):
-    # write test results to results.txt
-    with open(output_file, 'a') as f:
-        for result in results:
-            f.write(result + '\n')
-        f.write('\n')
+def export_results(batch_results, input_file=input_file, batch_size=batch_size):
+    for result in batch_results:
+        ins = results.insert().values(number=result[0], prime1=result[1][0][1],
+        prime2=result[1][0][2], ecm=result[1][0][3])
+        conn = db.connect()
+        r = conn.execute(ins)
     # delete batch from primeproducts.txt
     with open(input_file, 'r') as f:
         remaining_lines = f.readlines()[batch_size:]
@@ -42,32 +56,36 @@ def export_results(results, output_file=output_file, input_file=input_file, batc
 
 def ecm(n):
     for i in range(10):
+        if i > 3:
+            print(i)
         start = time()
         result = lenstra(n, 1000*10**i)
         speed = time() - start
         if result is not False:
-            return [result, speed]
-    return [result, speed]
+            p1, p2 = min(result, int(n/result)), max(result, int(n/result))
+            return [p1, p2, speed]
+    return [False, speed]
 
-algorithms = [ecm]
+algorithms = {'ecm':ecm}
 
 def speedtest(algorithms=algorithms, device=device):
     batch = import_batch()
-    results = []
-    for data in batch:
+    batch_results = []
+    for number in batch:
         algorithm_results = []
         for algorithm in algorithms:
-            result, speed = algorithm(data)
-            algorithm_results.append(str(algorithm) + ' : ' + str(result) + ' | ' + str(speed))
-        results.append(str(data) + ' | ' + ', '.join(algorithm_results) + ' | ' + str(datetime.utcnow()) + ' | ' + device)
-    export_results(results)
+            p1, p2, speed = algorithms[algorithm](number)
+            algorithm_results.append([algorithm, str(p1), str(p2), speed])
+        batch_results.append([str(number), algorithm_results])
+    export_results(batch_results)
+
+#########################################
 
 if __name__ == '__main__':
     file_length = file_len(input_file)
     print('===============================')
     print('running speedtest for %i numbers' % (file_length))
     print('input file: %s' % (input_file))
-    print('output file: %s' % (output_file))
     print('batch size: %s' % (batch_size))
     print('device: %s' % (device))
     print('===============================')
@@ -76,5 +94,5 @@ if __name__ == '__main__':
         speedtest()
         print('batch[' + str(i+1) + '/' + str(file_length//batch_size) + ']')
     print('finished...')
-    print('you can now view the results in the output_file')
+    print('you can now view the results in the database')
     print('===============================')
