64b66d9b84942696d5d6bbf4640fb1aa810699b2
EliasKomar <elias.komar@ctemplar.com>
2020-09-25 23:08:40 +0200

    only line number

--- speedtest.sh
+++ speedtest.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 len=$(cat primeproducts.txt | wc -l)
-len=${len:6}
 
 i=0
 cat primeproducts.txt | while read line; do
   { time python -m primefac -m=ecm $line; } 2>&1 | grep -e $line -e real >> ecm.txt;
   { time python -m primefac -m=mpqs $line; } 2>&1 | grep -e $line -e real >> mpqs.txt;
   i=$(($i+1))
-  echo "[$i/$len]"
+  echo "[$i/${len//[^0-9]/}]"
 done
