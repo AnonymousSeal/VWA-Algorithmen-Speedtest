8c35e84e66e396cfdea48d28897724d9196e4bb0
EliasKomar <elias.komar@ctemplar.com>
2020-10-27 15:17:17 +0100

    get numbers function and plots are saved as images

--- plot.py
+++ plot.py
@@ -1,15 +1,15 @@
 from matplotlib import pyplot as plt
 import math
 
-numbers = [math.log(int(line.rstrip()),10) for line in open('primeproducts.txt')]
-
-nums = [line.rstrip() for line in open('ecm.txt')]
-nums = nums[::2]
-numbers = []
-for num in nums:
-    index = num.find(':')
-    number = math.log(int(num[:index]), 10)
-    numbers.append(number)
+def get_numbers(filename):
+    nums = [line.rstrip() for line in open(filename)]
+    nums = nums[::2]
+    numbers = []
+    for num in nums:
+        index = num.find(':')
+        number = math.log(int(num[:index]), 10)
+        numbers.append(number)
+    return numbers
 
 def get_timings(filename):
     timings = [line.rstrip() for line in open(filename)]
@@ -22,17 +22,27 @@ def get_timings(filename):
         time_float.append(float(minutes)*60 + float(seconds))
     return time_float
 
-ecm_timings = get_timings('ecm.txt')
-mpqs_timings = get_timings('mpqs.txt')
-
-print(ecm_timings)
-print(mpqs_timings)
+def array_log(inputlist):
+    return [math.log(elem) for elem in inputlist]
 
 fig, ax = plt.subplots()
 ax.set_title("Speedtest")
-ax.set_xlabel("Length of number (log(n))")
-ax.set_ylabel("Time taken (seconds)")
-ax.scatter(numbers, ecm_timings, label='ecm')
-ax.scatter(numbers, mpqs_timings, label='mpqs')
+ax.set_xlabel("Length of number (log(number))")
+ax.set_ylabel("Time taken (log(seconds))")
+ax.scatter(get_numbers('results/ecm.txt'), array_log(get_timings('results/ecm.txt')), label='ecm')
+ax.scatter(get_numbers('results/mpqs.txt'), array_log(get_timings('results/mpqs.txt')), label='mpqs')
+ax.scatter(get_numbers('results/basic.txt'), array_log(get_timings('results/basic.txt')), label='basic')
 ax.legend(loc='upper left')
-plt.show()
+fig.savefig('results/plot0.png', dpi=1000, transperent=True)
+fig.savefig('results/plot0.svg')
+
+fig, ax = plt.subplots()
+ax.set_title("Speedtest")
+ax.set_xlabel("Length of number (log(number))")
+ax.set_ylabel("Time taken (seconds)")
+ax.scatter(get_numbers('results/ecm.txt'), get_timings('results/ecm.txt'), label='ecm')
+ax.scatter(get_numbers('results/mpqs.txt'), get_timings('results/mpqs.txt'), label='mpqs')
+ax.scatter(get_numbers('results/basic.txt'), get_timings('results/basic.txt'), label='basic')
+ax.legend(loc='upper left')
+fig.savefig('results/plot1.png', dpi=1000, transperent=True)
+fig.savefig('results/plot1.svg')
